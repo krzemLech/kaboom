9e88a557dc2163eaa3d33df061c42aacf1562d4c
krzemLech <krzemmo@gmail.com>
2025-01-03 18:50:33 +0100

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: netlify(),
 });
